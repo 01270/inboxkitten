e0f738aef324f3d9eda2f7ad1391cd7bb2820f71
01270 <100817706+01270@users.noreply.github.com>
2022-10-03 08:43:44 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'This package can make temp email from https://inboxkitten.com/'
 LONG_DESCRIPTION = 'This package can make temp email from https://inboxkitten.com/'
 
@@ -18,6 +18,9 @@ setup(
     author="Tufaah",
     author_email="yazanemails@gmail.com",
     description=DESCRIPTION,
+    project_urls={
+        "Github": "https://github.com/01270/inboxkitten",
+    },
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
